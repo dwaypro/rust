4d26784e044472ed67e4e1c042fe251c3fda05ec
unknown <dangrantway@gmail.com>
2018-12-23 12:38:35 -0700

    enums and Option<T>

--- structsenums/enums/enums/src/main.rs
+++ structsenums/enums/enums/src/main.rs
@@ -1,7 +1,14 @@
+// use std::option::Option;
+
 enum IpAddrKind{
     V4(u8, u8, u8, u8),
     V6(String),
 }
+
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
 
 fn route(ip_type: IpAddrKind){
 
@@ -15,6 +22,13 @@ fn main() {
 
     let home = IpAddrKind::V4(127,0,0,1);
     let loopback = IpAddrKind::V6(String::from("::1"));
- 
     
+    route(home);
+    
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+     println!("Testerooo {:#?}", absent_number);   
 }
