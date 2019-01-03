551cd6784b074483320095275c52cde7d9c2e8ae
unknown <dangrantway@gmail.com>
2019-01-02 20:52:12 -0700

    update functions

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-
-    // let mut largest = number_list[0];
+    let number_list2 = vec![35,36,37,38,39,40];
+    // let mut largest = number_list[0]
 
     // for number in number_list {
     //     if number > largest {
@@ -12,6 +12,7 @@ fn main() {
     // println!("The largest number is {}", largest);
 
     largest(&number_list);
+    largest(&number_list2);
 }
 
 
