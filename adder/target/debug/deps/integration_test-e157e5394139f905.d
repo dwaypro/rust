52c16a61e27462b22d423a6ed5f883eadfae507d
unknown <dangrantway@gmail.com>
2019-01-03 19:32:07 -0700

    update tests

---
+++ adder/target/debug/deps/integration_test-e157e5394139f905.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\adder\target\debug\deps\integration_test-e157e5394139f905.exe: tests\integration_test.rs
+
+C:\Users\Daniel\projects\rust\rust\adder\target\debug\deps\integration_test-e157e5394139f905.d: tests\integration_test.rs
+
+tests\integration_test.rs:
---
+++ adder/target/debug/integration_test-e157e5394139f905.d
@@ -0,0 +1,1 @@
+C:\Users\Daniel\projects\rust\rust\adder\target\debug\integration_test-e157e5394139f905.exe: C:\Users\Daniel\projects\rust\rust\adder\src\lib.rs C:\Users\Daniel\projects\rust\rust\adder\tests\integration_test.rs
