8a676c88ca51f9eb7e13065e01da3c905cd5b425
unknown <dangrantway@gmail.com>
2019-01-07 20:24:00 -0700

    first test

---
+++ grep/grep/target/debug/deps/grep-02ef1bdb7970bd79.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\grep\grep\target\debug\deps\grep-02ef1bdb7970bd79.exe: src\lib.rs
+
+C:\Users\Daniel\projects\rust\rust\grep\grep\target\debug\deps\grep-02ef1bdb7970bd79.d: src\lib.rs
+
+src\lib.rs:
---
+++ grep/grep/target/debug/grep-02ef1bdb7970bd79.d
@@ -0,0 +1,1 @@
+C:\Users\Daniel\projects\rust\rust\grep\grep\target\debug\grep-02ef1bdb7970bd79.exe: C:\Users\Daniel\projects\rust\rust\grep\grep\src\lib.rs
