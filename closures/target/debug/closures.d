7acb638ccaf20cb292690e2a1beaa92f1cf7fee3
unknown <dangrantway@gmail.com>
2019-01-09 20:14:32 -0700

    iterators

--- closures/target/debug/closures.d
+++ closures/target/debug/closures.d
@@ -1,1 +1,1 @@
-c:\Users\Daniel\projects\rust\rust\closures\target\debug\closures.pdb: c:\Users\Daniel\projects\rust\rust\closures\src\main.rs
+C:\Users\Daniel\projects\rust\rust\closures\target\debug\closures.pdb: C:\Users\Daniel\projects\rust\rust\closures\src\main.rs
