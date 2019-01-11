41af8deb1d05df6f017a40151f3c9d1762842678
unknown <dangrantway@gmail.com>
2019-01-10 22:01:37 -0700

    derefences

---
+++ smartpointers/target/debug/deps/smartpointers-84e82d65d3f6f4d9.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\smartpointers\target\debug\deps\smartpointers-84e82d65d3f6f4d9.rmeta: src\main.rs
+
+C:\Users\Daniel\projects\rust\rust\smartpointers\target\debug\deps\smartpointers-84e82d65d3f6f4d9.d: src\main.rs
+
+src\main.rs:
---
+++ smartpointers/target/debug/deps/smartpointers-b26914ff2953500a.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\smartpointers\target\debug\deps\smartpointers-b26914ff2953500a.exe: src\main.rs
+
+C:\Users\Daniel\projects\rust\rust\smartpointers\target\debug\deps\smartpointers-b26914ff2953500a.d: src\main.rs
+
+src\main.rs:
