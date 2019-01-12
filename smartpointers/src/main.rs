e370f79953cf4daf57e78da6f74b55a9d4a8bd7c
unknown <dangrantway@gmail.com>
2019-01-11 21:13:55 -0700

    smart pointers

--- smartpointers/src/main.rs
+++ smartpointers/src/main.rs
@@ -16,15 +16,29 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+struct CustomSmartPointer{
+    data: String,
+}
+
+impl Drop for CustomSmartPointer{
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);       
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
     let m = MyBox::new(String::from("Rust"));
 
     hello(&m);
-    
+
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    let c = CustomSmartPointer { data: String::from("my stuff")};
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
 }
 
 fn hello(name: &str){
