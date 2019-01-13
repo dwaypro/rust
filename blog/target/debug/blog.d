b3a357181734e9084024a17c58118db6001407cd
unknown <dangrantway@gmail.com>
2019-01-13 13:31:42 -0700

    blog crate practice, new hotkey mapping of create new file to explorer.newfile instead of workbench.action.files.newUntitledFile

---
+++ blog/target/debug/blog.d
@@ -0,0 +1,1 @@
+C:\Users\Daniel\projects\rust\rust\blog\target\debug\blog.pdb: C:\Users\Daniel\projects\rust\rust\blog\src\main.rs
---
+++ blog/target/debug/deps/blog-4f85e69ac7cdf9db.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\blog\target\debug\deps\blog-4f85e69ac7cdf9db.exe: src\main.rs
+
+C:\Users\Daniel\projects\rust\rust\blog\target\debug\deps\blog-4f85e69ac7cdf9db.d: src\main.rs
+
+src\main.rs:
