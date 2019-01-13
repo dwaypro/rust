cc1925ea697952069666151c0b861a2b3131e67a
unknown <dangrantway@gmail.com>
2019-01-12 18:18:47 -0700

    threads and editor settings

--- threadcommunication/src/main.rs
+++ threadcommunication/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    operate(15);
 }
+
+fn operate(a: i32) -> i32{
+    println!("hello!");
+    a
+}
+
+
---
+++ threadcommunication/target/debug/deps/threadcommunication-b35172e2db6490a4.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\threadcommunication\target\debug\deps\threadcommunication-b35172e2db6490a4.rmeta: src\main.rs
+
+C:\Users\Daniel\projects\rust\rust\threadcommunication\target\debug\deps\threadcommunication-b35172e2db6490a4.d: src\main.rs
+
+src\main.rs:
