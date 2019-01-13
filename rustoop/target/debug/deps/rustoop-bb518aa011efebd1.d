ca7724b7f1c6a4d6f67d2b9e19bf9845c3c1fac1
unknown <dangrantway@gmail.com>
2019-01-13 10:39:33 -0700

    more on traits and shared code

---
+++ rustoop/target/debug/deps/rustoop-bb518aa011efebd1.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\rustoop\target\debug\deps\rustoop-bb518aa011efebd1.rmeta: src\lib.rs
+
+C:\Users\Daniel\projects\rust\rust\rustoop\target\debug\deps\rustoop-bb518aa011efebd1.d: src\lib.rs
+
+src\lib.rs:
---
+++ rustoop/target/debug/librustoop.d
@@ -0,0 +1,1 @@
+C:\Users\Daniel\projects\rust\rust\rustoop\target\debug\librustoop.rmeta: C:\Users\Daniel\projects\rust\rust\rustoop\src\lib.rs C:\Users\Daniel\projects\rust\rust\rustoop\src\main.rs
