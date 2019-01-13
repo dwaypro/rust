7ab5a512b96a95d0c1a22cc1db55b70831b994d3
unknown <dangrantway@gmail.com>
2019-01-13 13:10:50 -0700

    if it walks like a duck and quacks like a duck then it mus be a duck

--- rustoop/src/lib.rs
+++ rustoop/src/lib.rs
@@ -2,12 +2,24 @@ pub trait Draw{
     fn draw(&self);
 }
 
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+//     where T: Draw {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
 }
 
-impl<T> Screen<T>
-    where T: Draw {
+impl Screen {
     pub fn run(&self) {
         for component in self.components.iter() {
             component.draw();
@@ -15,7 +27,7 @@ impl<T> Screen<T>
     }
 }
 
-pub struct Button{
+pub struct Button {
     pub width: u32,
     pub height: u32,
     pub label: String,
