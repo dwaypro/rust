95d88a3d7fbe6f47a5e506f4aedd6504272a7e99
unknown <dangrantway@gmail.com>
2019-01-13 18:59:57 -0700

    rust single threaded webserver

---
+++ webserver/target/debug/deps/webserver-75eb7b8777ff88b2.d
@@ -0,0 +1,5 @@
+C:\Users\Daniel\projects\rust\rust\webserver\target\debug\deps\webserver-75eb7b8777ff88b2.rmeta: src\main.rs
+
+C:\Users\Daniel\projects\rust\rust\webserver\target\debug\deps\webserver-75eb7b8777ff88b2.d: src\main.rs
+
+src\main.rs:
---
+++ webserver/target/debug/libwebserver.d
@@ -0,0 +1,1 @@
+C:\Users\Daniel\projects\rust\rust\webserver\target\debug\libwebserver.rmeta: C:\Users\Daniel\projects\rust\rust\webserver\src\main.rs
